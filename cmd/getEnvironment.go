6f9f7e28a2fdc4c58c87e782a7e0756414baa43e
Curtis Myzie <curtis@fugue.co>
2019-08-13 07:25:01 -0400

    Improve get environment output

--- cmd/getEnvironment.go
+++ cmd/getEnvironment.go
@@ -2,15 +2,18 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/fugue/fugue-client/client/environments"
+	"github.com/fugue/fugue-client/client/scans"
 	"github.com/fugue/fugue-client/format"
 	"github.com/spf13/cobra"
 )
 
 type Item struct {
-	Key   string
-	Value interface{}
+	Attribute string
+	Value     interface{}
 }
 
 // NewGetEnvironmentCommand returns a command that retrieves environment details
@@ -30,26 +33,42 @@ func NewGetEnvironmentCommand() *cobra.Command {
 
 			resp, err := client.Environments.GetEnvironment(params, auth)
 			CheckErr(err)
-
 			env := resp.Payload
+
+			maxItems := int64(1)
+			listScansParams := scans.NewListScansParams()
+			listScansParams.EnvironmentID = args[0]
+			listScansParams.MaxItems = &maxItems
+			lsResp, err := client.Scans.ListScans(listScansParams, auth)
+			CheckErr(err)
+
+			var lastScanID string
+			if len(lsResp.Payload.Items) > 0 {
+				lastScanID = lsResp.Payload.Items[0].ID
+			}
+
+			families := strings.Join(env.ComplianceFamilies, ",")
+			nextScanAt := time.Unix(env.NextScanAt, 0)
+			lastScanAt := time.Unix(env.LastScanAt, 0)
 
 			items := []interface{}{
 				Item{"ID", env.ID},
 				Item{"Name", env.Name},
 				Item{"Provider", env.Provider},
 				Item{"ScanInterval", env.ScanInterval},
-				Item{"LastScanAt", env.LastScanAt},
-				Item{"NextScanAt", env.NextScanAt},
+				Item{"LastScanID", lastScanID},
+				Item{"LastScanAt", lastScanAt.Format(time.RFC3339)},
+				Item{"NextScanAt", nextScanAt.Format(time.RFC3339)},
 				Item{"ScanStatus", env.ScanStatus},
-				Item{"Compliance Families", env.ComplianceFamilies},
+				Item{"ComplianceFamilies", families},
 				Item{"Drift", env.Drift},
 				Item{"Remediation", env.Remediation},
 			}
 
 			table, err := format.Table(format.TableOpts{
 				Rows:       items,
-				Columns:    []string{"Key", "Value"},
-				ShowHeader: false,
+				Columns:    []string{"Attribute", "Value"},
+				ShowHeader: true,
 			})
 			CheckErr(err)
 
