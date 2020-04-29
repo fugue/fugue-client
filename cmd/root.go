e2b79f2ce3631ce61825e00f0a6dc909d2c8940f
Ricardo Ferraz Leal <rleal@fugue.co>
2020-04-29 17:26:49 -0400

    Better error handing

--- cmd/root.go
+++ cmd/root.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"strings"
 
 	homedir "github.com/mitchellh/go-homedir"
@@ -19,26 +20,20 @@ const (
 )
 
 var cfgFile string
-var captureJSON bool
+var outputFormat string
 
 //jsonPositionToShow When assigned in the children commands gets the nth Json in the output
 var jsonPositionToShow int = -1
 
 func isOutputJSON() bool {
-	// We need to to os Args because rootCmd.Execute() has not run yet
+	// We need to check the command line Args because rootCmd.Execute() has not run yet
 	argsWithoutProg := os.Args[1:]
-	for _, elem := range argsWithoutProg {
-		if strings.ToLower(elem) == "--json" {
-			return true
-		}
-	}
-	return false
+	matched, _ := regexp.MatchString(`(?i)--output json`, strings.Join(argsWithoutProg, " "))
+	return matched
 }
 
-func printJSONOutput(out []byte) {
-
+func jsonOutput(out []byte) string {
 	outArray := bytes.Split(out, []byte("\n"))
-
 	var jsonArray []string
 	for _, v := range outArray {
 		var js map[string]interface{}
@@ -57,7 +52,7 @@ func printJSONOutput(out []byte) {
 	if err := json.Indent(elemToPrintIndented, []byte(elemToPrint), "", "  "); err != nil {
 		panic(err)
 	}
-	fmt.Println(elemToPrintIndented.String())
+	return elemToPrintIndented.String()
 }
 
 // rootCmd represents the base command when called without any subcommands
@@ -65,10 +60,40 @@ var rootCmd = &cobra.Command{
 	Use:   "fugue",
 	Short: "Fugue API Client",
 	Long:  ``,
+	// This is a hack to check the flag output is valid.
+	// wait for this to be merged: https://github.com/spf13/pflag/issues/236
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		validOutputs := []string{"table", "json"}
+		valid := func() bool {
+			for _, validOutput := range validOutputs {
+				if outputFormat == validOutput {
+					return true
+				}
+			}
+			return false
+		}
+		if !valid() {
+			Fatal(fmt.Sprintf("Value '%s' is invalid for flag 'output'. Valid values are: %v",
+				outputFormat, validOutputs), DefaultErrorExitCode)
+		}
+	},
 
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+var rescueStdout, rStdout, wStdout *os.File
+var rescueStderr, rStderr, wStderr *os.File
+
+func getRedirectedOutput() ([]byte, []byte) {
+	wStdout.Close()
+	wStderr.Close()
+	out, _ := ioutil.ReadAll(rStdout)
+	err, _ := ioutil.ReadAll(rStderr)
+	os.Stdout = rescueStdout
+	os.Stderr = rescueStderr
+	return out, err
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -76,25 +101,22 @@ var rootCmd = &cobra.Command{
 func Execute(version, commit string) {
 	rootCmd.Version = fmt.Sprintf("%s-%s", version, commit)
 
-	var rescueStdout, wStdout *os.File
-	var rescueStderr, rStderr, wStderr *os.File
+	rootCmd.SetOutput(os.Stderr)
+
 	if isOutputJSON() {
 		os.Setenv("DEBUG", "1")
 		rescueStdout, rescueStderr = os.Stdout, os.Stderr
-		_, wStdout, _ = os.Pipe()
+		rStdout, wStdout, _ = os.Pipe()
 		rStderr, wStderr, _ = os.Pipe()
 		os.Stdout, os.Stderr = wStdout, wStderr
 	}
 
 	CheckErr(rootCmd.Execute())
 
-	if isOutputJSON() && captureJSON {
-		wStdout.Close()
-		wStderr.Close()
-		err, _ := ioutil.ReadAll(rStderr)
-		os.Stdout = rescueStdout
-		os.Stderr = rescueStderr
-		printJSONOutput(err)
+	if isOutputJSON() {
+		_, err := getRedirectedOutput()
+		outStr := jsonOutput(err)
+		fmt.Println(outStr)
 	}
 }
 
@@ -103,7 +125,7 @@ func init() {
 
 	// Application global flags
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.fugue.yaml)")
-	rootCmd.PersistentFlags().BoolVar(&captureJSON, "json", false, "outputs the Fugue API JSON response")
+	rootCmd.PersistentFlags().StringVar(&outputFormat, "output", "table", "Use 'json' to access the Fugue API JSON response")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -132,11 +154,24 @@ func initConfig() {
 // Fatal prints the message (if provided) and then exits.
 func Fatal(msg string, code int) {
 	if len(msg) > 0 {
-		// add newline if needed
-		if !strings.HasSuffix(msg, "\n") {
-			msg += "\n"
+
+		if isOutputJSON() {
+			_, err := getRedirectedOutput()
+			outStr := jsonOutput(err)
+
+			if outStr != "" {
+				fmt.Fprintln(os.Stderr, outStr)
+			} else {
+				fmt.Fprintf(os.Stderr, `{"error": "%s"}\\n`, msg)
+			}
+		} else {
+			// add newline if needed
+			if !strings.HasSuffix(msg, "\n") {
+				msg += "\n"
+			}
+			fmt.Fprint(os.Stderr, msg)
 		}
-		fmt.Fprint(os.Stderr, msg)
+
 	}
 	os.Exit(code)
 }
